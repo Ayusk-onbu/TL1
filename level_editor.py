aed9b7710543e606f8fed769574c0c5ad8c80ff2
Hamada <k024g0103@g.neec.ac.jp>
2026-04-21 12:38:47 +0900

    TL_04

--- level_editor.py
+++ level_editor.py
@@ -13,11 +13,41 @@ bl_info = {
     "category": "Object"
 }
 
+# メニュー項目描画
+def draw_menu_manual(self, context):
+    self.layout.operator("wm.url_open_preset", text="Manual", icon='HELP')
+
+# トップバーの拡張メニュー
+class TOPBAR_MT_my_menu(bpy.types.Menu):
+    bl_idname = "TOPBAR_MT_my_menu"
+    bl_label = "MyMenu"
+    bl_description = "拡張メニュー by " + bl_info["author"]
+
+    def draw(self, context):
+        self.layout.operator("wm.url_open_preset",
+                             text="Manual", icon='HELP')
+        
+    def submenu(self, context):
+        self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
+
+classes = (
+    TOPBAR_MT_my_menu,
+)
+
 def register():
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
+    bpy.types.TOPBAR_MT_editor_menus.append(TOPBAR_MT_my_menu.submenu)
     print("LevelEditor is True")
     
 def unregister():
-    print("LevelEditor is False")
+    bpy.types.TOPBAR_MT_editor_menus.remove(TOPBAR_MT_my_menu.submenu)
+
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
     
+    print("LevelEditor is False")
+
 if __name__ == "__main__":
     register()
